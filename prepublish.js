940ed6aba4e730da315e3a623531f73828986009
Chris Ackerman <chris@topher.land>
2021-03-15 17:23:52 -0700

    removed execa

--- prepublish.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-const { version } = require('./package.json');
-const execa = require('execa');
-
-execa.commandSync(`git add .`, { stdio: 'inherit' });
-execa.commandSync(`git commit -m v${version}`, { stdio: 'inherit' });
-execa.commandSync(`git tag v${version}`, { stdio: 'inherit' });
-execa.commandSync(`git push --follow-tags`);
