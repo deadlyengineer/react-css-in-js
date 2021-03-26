2cceeef675ca651ccb1b985fbce13ddf3e101938
Chris Ackerman <chris@topher.land>
2021-03-26 00:15:49 -0700

    Fixed prepublish not running tests.

--- prepublishOnly.js
+++ prepublishOnly.js
@@ -3,6 +3,7 @@ const execa = require('execa');
 
 async function main() {
   await execa.command('npm i', { stdio: 'inherit' });
+  await execa.command('npm test', { stdio: 'inherit' });
   await execa.command('npm run build', { stdio: 'inherit' });
   await execa.command('git status -s', { all: true }).then(({ all: status }) => {
     if (status.trim() !== '') {
